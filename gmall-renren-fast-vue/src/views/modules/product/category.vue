8e9b14af78e5fc9b6cbc3b590432ace13024c8b0
muzimin <1367930183@qq.com>
2024-06-15 18:44:05 +0800

    feat: 配置网关和路径重写

---
+++ gmall-renren-fast-vue/src/views/modules/product/category.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    getMenus () {
+      this.$http({
+        url: this.$http.adornUrl('/category/list/tree'),
+        method: 'get'
+      }).then(data => {
+        console.log('成功获取到数据。。。', data)
+      })
+    }
+  },
+  created () {
+    this.getMenus()
+  }
+}
+</script>
+
+<style>
+
+</style>
