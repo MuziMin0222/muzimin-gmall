355de0481a45d1565a593425a7a369d43165db11
MuziMin0222 <1367930183@qq.com>
2024-08-21 16:55:10 +0800

    feat[前端]：实现规则校验功能

--- gmall-renren-fast-vue/src/views/modules/product/brand-add-or-update.vue
+++ gmall-renren-fast-vue/src/views/modules/product/brand-add-or-update.vue
@@ -28,7 +28,7 @@
         <el-input v-model="dataForm.firstLetter" placeholder="检索首字母"></el-input>
       </el-form-item>
       <el-form-item label="排序" prop="sort">
-        <el-input v-model="dataForm.sort" placeholder="排序"></el-input>
+        <el-input v-model.number="dataForm.sort" placeholder="排序"></el-input>
       </el-form-item>
     </el-form>
     <span slot="footer" class="dialog-footer">
@@ -51,9 +51,9 @@ export default {
         name: '',
         logo: '',
         descript: '',
-        showStatus: '',
+        showStatus: 1,
         firstLetter: '',
-        sort: ''
+        sort: 0
       },
       dataRule: {
         name: [
@@ -69,10 +69,32 @@ export default {
           {required: true, message: '显示状态', trigger: 'blur'}
         ],
         firstLetter: [
-          {required: true, message: '检索首字母不能为空', trigger: 'blur'}
+          {
+            validator: (rule, value, callback) => {
+              if (value === '') {
+                callback(new Error('首字母必须填写'))
+              } else if (!/^[a-zA-Z]$/.test(value)) {
+                callback(new Error('首字母必须a-z或者A-Z之间'))
+              } else {
+                callback()
+              }
+            },
+            trigger: 'blur'
+          }
         ],
         sort: [
-          {required: true, message: '排序不能为空', trigger: 'blur'}
+          {
+            validator: (rule, value, callback) => {
+              if (value === '') {
+                callback(new Error('排序字段必须填写'))
+              } else if (!Number.isInteger(value) || value < 0) {
+                callback(new Error('排序字段必须是一个大于等于0的整数'))
+              } else {
+                callback()
+              }
+            },
+            trigger: 'blur'
+          }
         ]
       }
     }
