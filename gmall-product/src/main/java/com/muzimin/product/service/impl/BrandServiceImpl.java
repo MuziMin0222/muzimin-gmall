c1896db7107073e17d43ce14c423f9bfe2d4260c
MuziMin0222 <1367930183@qq.com>
2024-09-09 17:14:19 +0800

    feat[后端]：品牌管理中的模糊查询

--- gmall-product/src/main/java/com/muzimin/product/service/impl/BrandServiceImpl.java
+++ gmall-product/src/main/java/com/muzimin/product/service/impl/BrandServiceImpl.java
@@ -1,7 +1,9 @@
 package com.muzimin.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,6 +13,7 @@ import com.muzimin.common.utils.Query;
 import com.muzimin.product.dao.BrandDao;
 import com.muzimin.product.entity.BrandEntity;
 import com.muzimin.product.service.BrandService;
+import org.springframework.util.StringUtils;
 
 
 @Service("brandService")
@@ -18,9 +21,16 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        // 1、获取Key
+        String key = (String) params.get("key");
+        QueryWrapper<BrandEntity> wrapper = new QueryWrapper<>();
+        if (!StringUtils.isEmpty(key)) {
+            wrapper.eq("brand_id", key).or().like("name", key);
+        }
+
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
-                new QueryWrapper<BrandEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
