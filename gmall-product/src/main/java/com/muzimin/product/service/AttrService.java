fd3a3c9607e9e0b28abb05af54bbfe2aceaaa80a
MuziMin0222 <1367930183@qq.com>
2024-10-12 16:14:26 +0800

    feat[后端端]：级联保存属性分组和关联关系功能

--- gmall-product/src/main/java/com/muzimin/product/service/AttrService.java
+++ gmall-product/src/main/java/com/muzimin/product/service/AttrService.java
@@ -3,6 +3,7 @@ package com.muzimin.product.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.muzimin.common.utils.PageUtils;
 import com.muzimin.product.entity.AttrEntity;
+import com.muzimin.product.vo.AttrVo;
 
 import java.util.Map;
 
@@ -16,5 +17,7 @@ import java.util.Map;
 public interface AttrService extends IService<AttrEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    void saveAttr(AttrVo attr);
 }
 
--- gmall-product/src/main/java/com/muzimin/product/service/impl/AttrServiceImpl.java
+++ gmall-product/src/main/java/com/muzimin/product/service/impl/AttrServiceImpl.java
@@ -1,7 +1,14 @@
 package com.muzimin.product.service.impl;
 
+import com.muzimin.product.dao.AttrAttrgroupRelationDao;
+import com.muzimin.product.entity.AttrAttrgroupRelationEntity;
+import com.muzimin.product.vo.AttrVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,10 +18,14 @@ import com.muzimin.common.utils.Query;
 import com.muzimin.product.dao.AttrDao;
 import com.muzimin.product.entity.AttrEntity;
 import com.muzimin.product.service.AttrService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
+
+    @Autowired
+    AttrAttrgroupRelationDao relationDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +35,21 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         );
 
         return new PageUtils(page);
+    }
+
+    @Transactional
+    @Override
+    public void saveAttr(AttrVo attr) {
+        AttrEntity entity = new AttrEntity();
+        BeanUtils.copyProperties(attr, entity);
+        //1、保存基本数据
+        this.save(entity);
+
+        // 2、 保存关联关系
+        AttrAttrgroupRelationEntity attrgroupRelationEntity = new AttrAttrgroupRelationEntity();
+        attrgroupRelationEntity.setAttrGroupId(attr.getAttrGroupId());
+        attrgroupRelationEntity.setAttrId(entity.getAttrId());
+        relationDao.insert(attrgroupRelationEntity);
     }
 
 }
